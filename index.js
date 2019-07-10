da1d260c0e3482e903ebd027aa8bf16c61c4c942
Thomas Theiner <thomas.theiner@gmx.de>
2019-07-10 16:19:34 +0200

    More streams, new client version, update handling

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ let client = new Client();
 let jsdom = require('jsdom');
 let { JSDOM } = jsdom;
 
+let strategies = require('./strategies');
+
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -37,45 +39,182 @@ app.listen(port, () => {
    console.log(`Server listening on port ${port} ...`);
 });
 
-// API
-let stationData = [{
+
+let stations = [{
          id: 1,
          name: "Rock Antenne Heavy Metal",
          src: "https://mp3channels.webradio.de/heavy-metal?&aw_0_1st.playerid=RockAntenneWebPlayer&aw_0_1st.listenerid=80423ecc532e6d7ebfe863577b38f2fc&aw_0_1st.skey=1562653242&aw_0_1st.gpslat=51.514&aw_0_1st.gpslong=7.476&aw_0_req.gdpr=true&aw_0_1st.spotcom=%5B%5D",
-         title: "Testinterpret - Testsong",
-         active: true
+         strategy: new strategies.RockAntenneStrategy("heavy-metal")
       }, {
          id: 2,
          name: "Wacken Radio",
          src: "https://de-hz-fal-stream02.rautemusik.fm/wackenradio",
-         title: "Testinterpret - Testsong",
-         active: true
+         strategy: new strategies.WackenRadioStrategy()
       }, {
          id: 3,
          name: "Metal Hammer",
          src: "https://metal-hammer.stream.laut.fm/metal-hammer",
-         title: "Testinterpret - Testsong",
-         active: true
+         strategy: new strategies.LautFMStrategy("metal-hammer")
       }, {
          id: 4,
          name: "Up The Irons",
          src: "https://uptheirons.stream.laut.fm/up_the_irons",
-         title: "Testinterpret - Testsong",
-         active: true
+         strategy: new strategies.LautFMStrategy("up_the_irons")
       }, {
          id: 5,
-         name: "Radio 91.2 Lokalradio",
-         src: "http://dg-ais-eco-http-fra-eco-cdn.cast.addradio.de/radio912/live/mp3/high?ar-distributor=f0b7",
-         title: "Testinterpret - Testsong",
-         active: true
+         name: "Metal Only",
+         src: "http://server1.blitz-stream.de:4400/;080794759699378stream.nsv",
+         strategy: new strategies.RadioDEStrategy(4696)
       }, {
          id: 6,
+         name: "Stahlradio",
+         src: "http://54.36.135.248:5280/stream",
+         strategy: new strategies.RadioDEStrategy(10416)
+      }, {
+         id: 7,
+         name: "Rockerportal",
+         src: "https://rockerportal.stream.laut.fm/rockerportal",
+         strategy: new strategies.LautFMStrategy("rockerportal")
+      }, {
+         id: 8,
+         name: "Motorbreath",
+         src: "https://motorbreath.stream.laut.fm/motorbreath",
+         strategy: new strategies.LautFMStrategy("motorbreath")
+      }, {
+         id: 9,
+         name: "Metalstation",
+         src: "https://metalstation.stream.laut.fm/metalstation",
+         strategy: new strategies.LautFMStrategy("metalstation")
+      }, {
+         id: 10,
+         name: "Metal FM",
+         src: "https://metal-fm-com.stream.laut.fm/metal-fm-com",
+         strategy: new strategies.LautFMStrategy("metal-fm-com")
+      }, {
+         id: 11,
          name: "Radio 91.2 80er Radio",
          src: "http://dg-ais-eco-http-fra-eco-cdn.cast.addradio.de/rnrw-014D/dein80er/high/stream.mp3?ar-purpose=web&ar-distributor=f0b7&sid=014d",
-         title: "Testinterpret - Testsong",
-         active: true
-}]
+         strategy: new strategies.NRWLokalRadioStrategy(1001)
+      }, {
+         id: 12,
+         name: "R.SH 80er",
+         src: "http://regiocast.hoerradar.de/rsh-80er-mp3-hq",
+         strategy: new strategies.RSHStrategy(5)
+      }, {
+         id: 13,
+         name: "Antenne Bayern 80er",
+         src: "http://mp3channels.webradio.antenne.de/80er-kulthits",
+         strategy: new strategies.RadioDEStrategy(9123)
+      }, {
+         id: 14,
+         name: "Radio Regenbogen 80er",
+         src: "http://streams.regenbogen.de/rr-80er-128-mp3",
+         strategy: new strategies.RegenbogenStrategy("80er")
+      }, {
+         id: 15,
+         name: "FFH 80er",
+         src: "http://mp3.ffh.de/ffhchannels/hq80er.mp3",
+         strategy: new strategies.RadioDEStrategy(9710)
+      }, {
+         id: 16,
+         name: "Laut FM 80er",
+         src: "https://80er.stream.laut.fm/80er",
+         strategy: new strategies.LautFMStrategy("80er")
+      }, {
+         id: 17,
+         name: "Best of 80s - Jede Stunde ein neues Jahr",
+         src: "https://bestof80s.stream.laut.fm/best_of_80s",
+         strategy: new strategies.LautFMStrategy("best_of_80s")
+      }, {
+         id: 18,
+         name: "0-24 80er Pop Rock",
+         src: "https://0-2480erpoprock.stream.laut.fm/0-24_80er_pop_rock",
+         strategy: new strategies.LautFMStrategy("0-24_80er_pop_rock")
+      }, {
+         id: 19,
+         name: "Move On",
+         src: "https://moveon.stream.laut.fm/move_on",
+         strategy: new strategies.LautFMStrategy("move_on")
+      }, {
+         id: 20,
+         name: "Just 80s",
+         src: "https://just80s.stream.laut.fm/just80s",
+         strategy: new strategies.LautFMStrategy("just80s")
+      }, {
+         id: 21,
+         name: "RPR1 Best of 80s",
+         src: "http://streams.rpr1.de/rpr-80er-128-mp3?usid=0-0-H-M-D-06",
+         strategy: new strategies.RadioDEStrategy(9304)
+      }, {
+         id: 22,
+         name: "Wunschradio FM 80er",
+         src: "http://server74.radiostreamserver.de/wunschradio-80er.mp3",
+         strategy: new strategies.WunschradioFMStrategy("80er")
+      }, {
+         id: 23,
+         name: "RPR1 Neue Deutsche Welle",
+         src: "http://streams.rpr1.de/rpr-ndw-128-mp3?usid=0-0-H-M-D-06",
+         strategy: new strategies.RadioDEStrategy(41162)
+      }, {
+         id: 24,
+         name: "80s80s NDW",
+         src: "http://80s80s.hoerradar.de/80s80s-ndw-mp3-mq?sABC=5q25r1n2%230%232000r3268p2816383571rn5877123r83%23enqvbqr&amsparams=playerid:radiode;skey:1562763682",
+         strategy: new strategies.RadioDEStrategy(41403)
+      }, {
+         id: 25,
+         name: "1A NDW",
+         src: "http://1a-ndw.radionetz.de:8000/1a-ndw.mp3",
+         strategy: new strategies.RadioDEStrategy(110228)
+      }, {
+         id: 26,
+         name: "Radio 91.2 Lokalradio",
+         src: "http://dg-ais-eco-http-fra-eco-cdn.cast.addradio.de/radio912/live/mp3/high?ar-distributor=f0b7",
+         strategy: new strategies.NRWLokalRadioStrategy(6)
+      }, {
+         id: 27,
+         name: "R.SH",
+         src: "http://regiocast.hoerradar.de/rsh128",
+         strategy: new strategies.RSHStrategy(1)
+      }, {
+         id: 28,
+         name: "1Live",
+         src: "http://edge-2016.fra-lg.cdn.addradio.net/wdr/1live/live/mp3/128/stream.mp3?ar-distributor=f0a0",
+         strategy: new strategies.RadioDEStrategy(1382)
+      }, {
+         id: 29,
+         name: "WDR2 - Ruhrgebiet",
+         src: "http://edge-2019.fra-lg.cdn.addradio.net/wdr/wdr2/ruhrgebiet/mp3/128/stream.mp3?ar-distributor=f0a0",
+         strategy: new strategies.RadioDEStrategy(9773)
+      }, {
+         id: 30,
+         name: "SWR3",
+         src: "http://edge-2025.dus-lg.cdn.addradio.net/swr/swr3/live/mp3/128/stream.mp3?ar-distributor=f0a0",
+         strategy: new strategies.RadioDEStrategy(2275)
+      }, {
+         id: 31,
+         name: "NDR2",
+         src: "http://edge-203a.dus-lg.cdn.addradio.net/ndr/ndr2/niedersachsen/mp3/128/stream.mp3?ar-distributor=f0a0",
+         strategy: new strategies.RadioDEStrategy(2262)
+      }, {
+         id: 32,
+         name: "Bayern3",
+         src: "http://edge-209f.dus-lg.cdn.addradio.net/br/br3/live/mp3/128/stream.mp3?ar-distributor=f0a0",
+         strategy: new strategies.RadioDEStrategy(2247)
+      }];
 
-app.get("/api/init", (req, res) => {
-  res.json(stationData);
+
+app.get("/api/init", async (req, res) => {
+  let result = [];
+  for(let station of stations) {
+    if(station.strategy) {
+      let title = await station.strategy.getTitle();
+
+      let stationData = {...station};
+      stationData.strategy = undefined;
+      stationData.title = title;
+      stationData.active = true;
+      result.push(stationData);
+    }
+  }
+  res.json(result);
 });
---
+++ strategies.js
@@ -0,0 +1,192 @@
+let Client = require('node-rest-client').Client;
+let client = new Client();
+let jsdom = require('jsdom');
+let { JSDOM } = jsdom;
+
+let headers = {
+   headers: {"Accept": "application/json"}
+}
+
+let HTMLheaders = {
+   headers: {"Accept": "text/html"}
+}
+
+function twoDigits(num) {
+   return ("0" + num).slice(-2);
+}
+
+class LautFMStrategy {
+   constructor(stationName) {
+      this.stationName = stationName;
+   }
+
+   getTitle() {
+      return new Promise((resolve, reject) => {
+         client.get(`http://api.laut.fm/station/${this.stationName}/current_song`, headers, (data, response) => {
+            let result = "Aktueller Titel ist unbekannt";
+            if(response.statusCode == 200) {
+               if(data.artist && data.title)
+                  result = data.artist.name + " - " + data.title;
+            }
+            resolve(result);
+         });
+      });
+   }
+}
+
+class RockAntenneStrategy {
+
+   constructor(stationName) {
+      this.stationName = stationName;
+   }
+
+   getTitle() {
+      return new Promise((resolve, reject) => {
+         client.get(`https://api.antenne.de/1.0.0/webradio/metadata/details?&apikey=10fa9dee55182f9d1cd359c787aec98a&streamingChannel=${this.stationName}`, headers, (data, response) => {
+            let result = "Aktueller Titel ist unbekannt";
+            if(response.statusCode == 200) {
+               if(data && data.object && data.object.now)
+                  result = data.object.now.artist + " - " + data.object.now.title;
+            }
+            resolve(result);
+         });
+      });
+   }
+}
+
+class WackenRadioStrategy {
+   constructor() {
+   }
+
+   getTitle() {
+      return new Promise((resolve, reject) => {
+         client.get(`http://www.rautemusik.fm/wackenradio/`, HTMLheaders , (data, response) => {
+            let result = "Aktueller Titel ist unbekannt";
+            if(response.statusCode == 200) {
+               let { document } = new JSDOM(data).window;
+               result = document.querySelector('.artist').textContent + " - " + document.querySelector('.title').textContent
+            }
+            resolve(result);
+         });
+      });
+   }
+}
+
+class NRWLokalRadioStrategy {
+   constructor(stationNr) {
+      this.stationNr = stationNr;
+   }
+
+   getTitle() {
+      return new Promise((resolve, reject) => {
+         client.get(`https://api-prod.nrwlokalradios.com/playlist/latest?station=${this.stationNr}&count=1`, headers, (data, response) => {
+            let result = "Aktueller Titel ist unbekannt";
+            if(response.statusCode == 200) {
+               if(data && data[0])
+                  result = data[0].artist + " - " + data[0].title;
+            }
+            resolve(result);
+         });
+      });
+   }
+}
+
+class RadioDEStrategy {
+   constructor(stationNr) {
+      this.stationNr = stationNr;
+   }
+
+   getTitle() {
+      return new Promise((resolve, reject) => {
+         client.get(`https://api.radio.de/info/v2/search/nowplaying?_=1562749410030&apikey=f69cc2b80499cb73c760ca2efcd8309a64f36585&numberoftitles=1&station=${this.stationNr}`, headers, (data, response) => {
+            let result = "Aktueller Titel ist unbekannt";
+            if(response.statusCode == 200) {
+               if(data && data[0])
+                  result = data[0].streamTitle;
+            }
+            resolve(result);
+         });
+      });
+   }
+}
+
+class RSHStrategy {
+   constructor(stationNr) {
+      this.stationNr = stationNr;
+   }
+
+   getTitle() {
+      return new Promise((resolve, reject) => {
+         client.get(`http://stream-service.loverad.io/v3/rsh?_=`, headers, (data, response) => {
+            let result = "Aktueller Titel ist unbekannt";
+            if(response.statusCode == 200) {
+               if(data && data[this.stationNr])
+                  result = data[this.stationNr]["artist_name"] + " - " + data[this.stationNr]["song_title"];
+            }
+            resolve(result);
+         });
+      });
+   }
+}
+
+class RegenbogenStrategy {
+   constructor(stationName) {
+      this.stationName = stationName;
+   }
+
+   getTitle() {
+      return new Promise((resolve, reject) => {
+         let datum = new Date();
+         let date = datum.getFullYear() + "/" + twoDigits(datum.getMonth()+1) + "/" + twoDigits(datum.getDate()) + "/" + twoDigits(datum.getHours());
+
+         client.get(`http://www.regenbogen.de/sites/default/files/nocache/${this.stationName}/${date}.json`, headers, (data, response) => {
+            let result = "Aktueller Titel ist unbekannt";
+            if(response.statusCode == 200) {
+               if(data && data.hour.length) {
+                  if(data.hour.length > 1) {
+                     let titleDate = new Date(data.hour[data.hour.length - 1].date);
+                     if(titleDate > date) {
+                        result = data.hour[data.hour.length - 2].artist + " - " + data.hour[data.hour.length - 2].title;
+                     } else {
+                        result = data.hour[data.hour.length - 1].artist + " - " + data.hour[data.hour.length - 1].title;
+                     }
+                  } else {
+                     result = data.hour[0].artist + " - " + data.hour[0].title;
+                  }
+               }
+            }
+            resolve(result);
+         });
+      });
+   }
+}
+
+class WunschradioFMStrategy {
+   constructor(stationName) {
+      this.stationName = stationName;
+   }
+
+   getTitle() {
+      return new Promise((resolve, reject) => {
+         client.get(`https://wunschradio.de/data/${this.stationName}.json`, headers, (data, response) => {
+            let result = "Aktueller Titel ist unbekannt";
+            if(response.statusCode == 200) {
+               if(data)
+                  result = data.artist + " - " + data.title;
+            }
+            resolve(result);
+         });
+      });
+   }
+}
+
+module.exports = {
+   LautFMStrategy: LautFMStrategy,
+   RockAntenneStrategy: RockAntenneStrategy,
+   WackenRadioStrategy: WackenRadioStrategy,
+   NRWLokalRadioStrategy: NRWLokalRadioStrategy,
+   RadioDEStrategy: RadioDEStrategy,
+   RSHStrategy: RSHStrategy,
+   RegenbogenStrategy: RegenbogenStrategy,
+   WunschradioFMStrategy: WunschradioFMStrategy
+}
