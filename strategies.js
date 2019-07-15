6c744ea7302b82d9e11292fe114e22c3555ca757
Thomas Theiner <thomas.theiner@gmx.de>
2019-07-15 09:11:36 +0200

    Added timeouts

--- strategies.js
+++ strategies.js
@@ -4,11 +4,23 @@ let jsdom = require('jsdom');
 let { JSDOM } = jsdom;
 
 let headers = {
-   headers: {"Accept": "application/json"}
+   headers: {"Accept": "application/json"},
+   requestConfig: {
+      timeout: 1000
+   },
+   responseConfig: {
+      timeout: 1000
+   }
 }
 
 let HTMLheaders = {
-   headers: {"Accept": "text/html"}
+   headers: {"Accept": "text/html"},
+   requestConfig: {
+      timeout: 1000
+   },
+   responseConfig: {
+      timeout: 1000
+   }
 }
 
 function twoDigits(num) {
@@ -21,13 +33,21 @@ class LautFMStrategy {
    }
 
    getTitle() {
+      let result = "Aktueller Titel ist unbekannt";
       return new Promise((resolve, reject) => {
-         client.get(`http://api.laut.fm/station/${this.stationName}/current_song`, headers, (data, response) => {
-            let result = "Aktueller Titel ist unbekannt";
+         let req = client.get(`http://api.laut.fm/station/${this.stationName}/current_song`, headers, (data, response) => {
             if(response.statusCode == 200) {
                if(data.artist && data.title)
                   result = data.artist.name + " - " + data.title;
             }
+            resolve(result);
+         });
+
+         req.on("requestTimeout", (request) => {
+            resolve(result);
+         });
+
+         req.on("responseTimeout", (request) => {
             resolve(result);
          });
       });
@@ -41,13 +61,21 @@ class RockAntenneStrategy {
    }
 
    getTitle() {
+      let result = "Aktueller Titel ist unbekannt";
       return new Promise((resolve, reject) => {
-         client.get(`https://api.antenne.de/1.0.0/webradio/metadata/details?&apikey=10fa9dee55182f9d1cd359c787aec98a&streamingChannel=${this.stationName}`, headers, (data, response) => {
-            let result = "Aktueller Titel ist unbekannt";
+         let req = client.get(`https://api.antenne.de/1.0.0/webradio/metadata/details?&apikey=10fa9dee55182f9d1cd359c787aec98a&streamingChannel=${this.stationName}`, headers, (data, response) => {
             if(response.statusCode == 200) {
                if(data && data.object && data.object.now)
                   result = data.object.now.artist + " - " + data.object.now.title;
             }
+            resolve(result);
+         });
+
+         req.on("requestTimeout", (request) => {
+            resolve(result);
+         });
+
+         req.on("responseTimeout", (request) => {
             resolve(result);
          });
       });
@@ -59,13 +87,21 @@ class WackenRadioStrategy {
    }
 
    getTitle() {
+      let result = "Aktueller Titel ist unbekannt";
       return new Promise((resolve, reject) => {
-         client.get(`http://www.rautemusik.fm/wackenradio/`, HTMLheaders , (data, response) => {
-            let result = "Aktueller Titel ist unbekannt";
+         let req = client.get(`http://www.rautemusik.fm/wackenradio/`, HTMLheaders , (data, response) => {
             if(response.statusCode == 200) {
                let { document } = new JSDOM(data).window;
                result = document.querySelector('.artist').textContent + " - " + document.querySelector('.title').textContent
             }
+            resolve(result);
+         });
+
+         req.on("requestTimeout", (request) => {
+            resolve(result);
+         });
+
+         req.on("responseTimeout", (request) => {
             resolve(result);
          });
       });
@@ -78,13 +114,21 @@ class NRWLokalRadioStrategy {
    }
 
    getTitle() {
+      let result = "Aktueller Titel ist unbekannt";
       return new Promise((resolve, reject) => {
-         client.get(`https://api-prod.nrwlokalradios.com/playlist/latest?station=${this.stationNr}&count=1`, headers, (data, response) => {
-            let result = "Aktueller Titel ist unbekannt";
+         let req = client.get(`https://api-prod.nrwlokalradios.com/playlist/latest?station=${this.stationNr}&count=1`, headers, (data, response) => {
             if(response.statusCode == 200) {
                if(data && data[0])
                   result = data[0].artist + " - " + data[0].title;
             }
+            resolve(result);
+         });
+
+         req.on("requestTimeout", (request) => {
+            resolve(result);
+         });
+
+         req.on("responseTimeout", (request) => {
             resolve(result);
          });
       });
@@ -98,13 +142,21 @@ class RadioDEStrategy {
    }
 
    getTitle() {
+      let result = "Aktueller Titel ist unbekannt";
       return new Promise((resolve, reject) => {
-         client.get(`https://api.radio.de/info/v2/search/nowplaying?_=1562749410030&apikey=f69cc2b80499cb73c760ca2efcd8309a64f36585&numberoftitles=2&station=${this.stationNr}`, headers, (data, response) => {
-            let result = "Aktueller Titel ist unbekannt";
+         let req = client.get(`https://api.radio.de/info/v2/search/nowplaying?_=1562749410030&apikey=f69cc2b80499cb73c760ca2efcd8309a64f36585&numberoftitles=2&station=${this.stationNr}`, headers, (data, response) => {
             if(response.statusCode == 200) {
                if(data && data[this.pos])
                   result = data[this.pos].streamTitle;
             }
+            resolve(result);
+         });
+
+         req.on("requestTimeout", (request) => {
+            resolve(result);
+         });
+
+         req.on("responseTimeout", (request) => {
             resolve(result);
          });
       });
@@ -117,13 +169,21 @@ class RSHStrategy {
    }
 
    getTitle() {
+      let result = "Aktueller Titel ist unbekannt";
       return new Promise((resolve, reject) => {
-         client.get(`http://stream-service.loverad.io/v3/rsh?_=`, headers, (data, response) => {
-            let result = "Aktueller Titel ist unbekannt";
+         let req = client.get(`http://stream-service.loverad.io/v3/rsh?_=`, headers, (data, response) => {
             if(response.statusCode == 200) {
                if(data && data[this.stationNr])
                   result = data[this.stationNr]["artist_name"] + " - " + data[this.stationNr]["song_title"];
             }
+            resolve(result);
+         });
+
+         req.on("requestTimeout", (request) => {
+            resolve(result);
+         });
+
+         req.on("responseTimeout", (request) => {
             resolve(result);
          });
       });
@@ -136,12 +196,12 @@ class RegenbogenStrategy {
    }
 
    getTitle() {
+      let result = "Aktueller Titel ist unbekannt";
       return new Promise((resolve, reject) => {
          let datum = new Date();
          let date = datum.getFullYear() + "/" + twoDigits(datum.getMonth()+1) + "/" + twoDigits(datum.getDate()) + "/" + twoDigits(datum.getHours());
 
-         client.get(`http://www.regenbogen.de/sites/default/files/nocache/${this.stationName}/${date}.json`, headers, (data, response) => {
-            let result = "Aktueller Titel ist unbekannt";
+         let req = client.get(`http://www.regenbogen.de/sites/default/files/nocache/${this.stationName}/${date}.json`, headers, (data, response) => {
             if(response.statusCode == 200) {
                if(data && data.hour.length) {
                   if(data.hour.length > 1) {
@@ -158,6 +218,14 @@ class RegenbogenStrategy {
             }
             resolve(result);
          });
+
+         req.on("requestTimeout", (request) => {
+            resolve(result);
+         });
+
+         req.on("responseTimeout", (request) => {
+            resolve(result);
+         });
       });
    }
 }
@@ -168,13 +236,21 @@ class WunschradioFMStrategy {
    }
 
    getTitle() {
+      let result = "Aktueller Titel ist unbekannt";
       return new Promise((resolve, reject) => {
-         client.get(`https://wunschradio.de/data/${this.stationName}.json`, headers, (data, response) => {
-            let result = "Aktueller Titel ist unbekannt";
+         let req = client.get(`https://wunschradio.de/data/${this.stationName}.json`, headers, (data, response) => {
             if(response.statusCode == 200) {
                if(data)
                   result = data.artist + " - " + data.title;
             }
+            resolve(result);
+         });
+
+         req.on("requestTimeout", (request) => {
+            resolve(result);
+         });
+
+         req.on("responseTimeout", (request) => {
             resolve(result);
          });
       });
@@ -187,13 +263,21 @@ class NinetiesStrategy {
    }
 
    getTitle() {
+      let result = "Aktueller Titel ist unbekannt";
       return new Promise((resolve, reject) => {
-         client.get(`https://iris-90s90s.loverad.io/flow.json?station=${this.stationNr}&offset=1&count=1&ts=1562854204648`, headers, (data, response) => {
-            let result = "Aktueller Titel ist unbekannt";
+         let req = client.get(`https://iris-90s90s.loverad.io/flow.json?station=${this.stationNr}&offset=1&count=1&ts=1562854204648`, headers, (data, response) => {
             if(response.statusCode == 200) {
                if(data && data.result && data.result.entry.length)
                   result = data.result.entry[0].song.entry[0].artist.entry[0].name + " - " + data.result.entry[0].song.entry[0].title;
             }
+            resolve(result);
+         });
+
+         req.on("requestTimeout", (request) => {
+            resolve(result);
+         });
+
+         req.on("responseTimeout", (request) => {
             resolve(result);
          });
       });
