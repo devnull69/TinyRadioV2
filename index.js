0c6d5fea537a8f8a6de601e06d73996017ca4ee2
Thomas Theiner <thomas.theiner@gmx.de>
2020-08-19 09:25:45 +0200

    New client without externalPauseRequest actions, added new station M1.FM 80er

--- index.js
+++ index.js
@@ -165,142 +165,148 @@ let stations = [{
          strategy: new strategies.LautFMStrategy("just80s")
       }, {
          id: 21,
+         name: "M1.FM 80er",
+         src: "https://tuner.m1.fm/80er.mp3",
+         scope: "80er",
+         strategy: new strategies.OnlineRadioBoxStrategy("de.m180er")
+      }, {
+         id: 22,
          name: "RPR1 Best of 80s",
          src: "http://streams.rpr1.de/rpr-80er-128-mp3?usid=0-0-H-M-D-06",
          scope: "80er",
          strategy: new strategies.OnlineRadioBoxStrategy("de.rpr280er"),
          titlecase: true
       }, {
-         id: 22,
+         id: 23,
          name: "Wunschradio FM 80er",
          src: "http://server74.radiostreamserver.de/wunschradio-80er.mp3",
          scope: "80er",
          strategy: new strategies.WunschradioFMStrategy("80er")
       }, {
-         id: 23,
+         id: 24,
          name: "RPR1 Neue Deutsche Welle",
          src: "http://streams.rpr1.de/rpr-ndw-128-mp3?usid=0-0-H-M-D-06",
          scope: "NDW",
          strategy: new strategies.RadioDEStrategy(41162, 1),
          titlecase: true
       }, {
-         id: 24,
+         id: 25,
          name: "80s80s NDW",
          src: "http://80s80s.hoerradar.de/80s80s-ndw-mp3-mq?sABC=5q25r1n2%230%232000r3268p2816383571rn5877123r83%23enqvbqr&amsparams=playerid:radiode;skey:1562763682",
          scope: "NDW",
          strategy: new strategies.OnlineRadioBoxStrategy("de.80s80sndw")
       }, {
-         id: 25,
+         id: 26,
          name: "1A NDW",
          src: "http://1a-ndw.radionetz.de:8000/1a-ndw.mp3",
          scope: "NDW",
          strategy: new strategies.RadioDEStrategy(110228, 0)
       }, {
-         id: 26,
+         id: 27,
          name: "Radio 91.2 Lokalradio",
          src: "https://radio912-ais-edge-3104-fra-eco-cdn.cast.addradio.de/radio912/live/mp3/high?ar-distributor=ffa0&_art=dj0yJmlwPTc5LjIxNi4yNi4xMzcmaWQ9aWNzY3hsLXR0emRyenVtYiZ0PTE1OTcxNDQ0NzUmcz03ODY2ZjI5YyNiZTA5ZDhiNDVmNmUyZWQyMTVhOTEwYzdmMzFjZWRmMw",
          scope: "Lokalradio",
          strategy: new strategies.NRWLokalRadioStrategy(6),
          titlecase: true
       }, {
-         id: 27,
+         id: 28,
          name: "R.SH",
          src: "http://regiocast.hoerradar.de/rsh128",
          scope: "Lokalradio",
          strategy: new strategies.RSHStrategy("R.SH Live")
       }, {
-         id: 28,
+         id: 29,
          name: "1Live",
          src: "https://wdr-edge-2020-fra-lg-cdn.cast.addradio.de/wdr/1live/live/mp3/128/stream.mp3?ar-distributor=f0a0&_art=dj0yJmlwPTc5LjIxNi4yNi4xMzcmaWQ9aWNzY3hsLXgyY3NzeWxsYiZ0PTE1OTcxNDQ5Njgmcz03ODY2ZjI5YyNlNTY5YmY0MWJlMGE4MzEzOGYzNzQwYjE1OTc1MzU0MA",
          scope: "Lokalradio",
          strategy: new strategies.RadioDEStrategy(1382, 0)
       }, {
-         id: 29,
+         id: 30,
          name: "WDR2 - Ruhrgebiet",
          src: "https://wdr-edge-201b-dus-lg-cdn.cast.addradio.de/wdr/wdr2/ruhrgebiet/mp3/128/stream.mp3?ar-distributor=f0a0&_art=dj0yJmlwPTc5LjIxNi4yNi4xMzcmaWQ9aWNzY3hsLWw2Y3NzeWxsYiZ0PTE1OTcxNDUwNzQmcz03ODY2ZjI5YyNhNWNjZmY3OTJlZWMwZWUwNTM0MTIxNDFjZmZlYTgwYw",
          scope: "Lokalradio",
          strategy: new strategies.RadioDEStrategy(9773, 0)
       }, {
-         id: 30,
+         id: 31,
          name: "SWR3",
          src: "https://swr-edge-2031-dus-lg-cdn.cast.addradio.de/swr/swr3/live/aac/96/stream.aac?ar-distributor=f0a0&_art=dj0yJmlwPTc5LjIxNi4yNi4xMzcmaWQ9aWNzY3hsLXZ5c2UzMmxsYiZ0PTE1OTcxNDUyMjAmcz03ODY2ZjI5YyM4YzBjMTcyZDViMTQ5YTNlMTI0YWQyZWUwMzNjOGJjYQ",
          scope: "Lokalradio",
          strategy: new strategies.RadioDEStrategy(2275, 0)
       }, {
-         id: 31,
+         id: 32,
          name: "NDR2",
          src: "https://ndr-edge-20b2-fra-lg-cdn.cast.addradio.de/ndr/ndr2/niedersachsen/mp3/128/stream.mp3?ar-distributor=f0a0&_art=dj0yJmlwPTc5LjIxNi4yNi4xMzcmaWQ9aWNzY3hsLWhpdW9jM2xsYiZ0PTE1OTcxNDUzMDMmcz03ODY2ZjI5YyNjMmY3ZGRhMGQwYTdjZWE0NGRmYzQ4YWRiOGE0ZTlmOA",
          scope: "Lokalradio",
          strategy: new strategies.RadioDEStrategy(2262, 0)
       }, {
-         id: 32,
+         id: 33,
          name: "Bayern3",
          src: "https://br-edge-2009-dus-lg-cdn.cast.addradio.de/br/br3/live/mp3/128/stream.mp3?ar-distributor=f0a0&_art=dj0yJmlwPTc5LjIxNi4yNi4xMzcmaWQ9aWNzY3hsLWRzcWR4d2tsYiZ0PTE1OTcxNDUzNjImcz03ODY2ZjI5YyM2ZTZiMjJjZjY5NGJiNWRiNzVmMDk2ODQ1ZWVkMjYyYg",
          scope: "Lokalradio",
          strategy: new strategies.RadioDEStrategy(2247, 0)
       }, {
-         id: 33,
+         id: 34,
          name: "R.SH 90er",
          src: "http://regiocast.hoerradar.de/rsh-90er-mp3-hq",
          scope: "90er",
          strategy: new strategies.RSHStrategy("R.SH 90er")
       }, {
-         id: 34,
+         id: 35,
          name: "Radio Regenbogen 90er",
          src: "http://streams.regenbogen.de/rr-90er-128-mp3",
          scope: "90er",
          strategy: new strategies.RegenbogenStrategy("90er"),
          titlecase: true
       }, {
-         id: 35,
+         id: 36,
          name: "FFH 90er",
          src: "http://mp3.ffh.de/ffhchannels/hq90er.mp3",
          scope: "90er",
          strategy: new strategies.RadioDEStrategy(22960, 0)
       }, {
-         id: 36,
+         id: 37,
          name: "Best of 90s",
          src: "https://bestof90s.stream.laut.fm/best_of_90s",
          scope: "90er",
          strategy: new strategies.LautFMStrategy("best_of_90s")
       }, {
-         id: 37,
+         id: 38,
          name: "Germanradio 90er",
          src: "http://germanradio.info:13300/;",
          scope: "90er",
          strategy: new strategies.OnlineRadioBoxStrategy("de.germanradioinfo90er")
       }, {
-         id: 38,
+         id: 39,
          name: "90s90s",
          src: "https://90s90s.hoerradar.de/90s90s-pop-mp3-hq?sABC=5q274229%230%23857psp88on39p1p3626785o66674n105%23ubzrcntr&context=fHA6LTE=&aw_0_1st.kuid=tsb7u9x35&aw_0_1st.ksg=[%22tow0oughf%22,%22tsrahp573%22,%22tow65wkpp%22,%22takaaes6u%22,%22ti5001c5h%22]&amsparams=playerid:homepage;skey:1562853929",
          scope: "90er",
          strategy: new strategies.NinetiesStrategy(141)
       }, {
-         id: 39,
+         id: 40,
          name: "Wunschradio FM 90er",
          src: "http://server74.radiostreamserver.de/wunschradio-90er.mp3",
          scope: "90er",
          strategy: new strategies.WunschradioFMStrategy("90er")
       }, {
-         id: 40,
+         id: 41,
          name: "M1.FM 90er",
          src: "http://tuner.m1.fm/90er.mp3",
          scope: "90er",
          strategy: new strategies.OnlineRadioBoxStrategy("de.m190er")
       }, {
-         id: 41,
+         id: 42,
          name: "Vienna.AT 90er",
          src: "http://webradio.vienna.at/vie-90er",
          scope: "90er",
          strategy: new strategies.OnlineRadioBoxStrategy("at.viennaat90er")
       }, {
-         id: 42,
+         id: 43,
          name: "Radio BOB! Metal",
          src: "http://bob.hoerradar.de/radiobob-metal-mp3-mq?sABC=5q2902po%230%23857psp88on39p1p3626785o66674n105%23enqvbqr&amsparams=playerid:radiode;skey:1562968779",
          scope: "heavymetal",
          strategy: new strategies.OnlineRadioBoxStrategy("de.bobsmetal")
       }, {
-         id: 43,
+         id: 44,
          name: "Metal Up Your Ass",
          src: "https://metal-up-your-ass.stream.laut.fm/metal-up-your-ass?ref=radiode&t302=2019-07-13_00-10-38&uuid=3935ca92-bf30-4e4b-a1d2-f8c0bf0ad7ce",
          scope: "heavymetal",
